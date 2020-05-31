36210c482d856f229a023bf1c6e7ff9a7c4ca459
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-31 10:18:31 +0430

    Fix

--- NetMeter/Program.cs
+++ NetMeter/Program.cs
@@ -13,30 +13,48 @@ namespace NetMeter
         static async Task Main(string[] args)
         {
 
-            var requestCount = int.Parse(args[0]);
-            var url = args[1];
+            var requestCount = 1000;//int.Parse(args[0]);
+            var url = "http://localhost:5000/api/sync";// args[1];
 
-            var taskList = new List<Task>();
+            var taskList = new List<Task<Tuple<int, long,int>>>();
             var s = new Stopwatch();
             s.Start();
             for (int i = 0; i < requestCount; i++)
             {
-               taskList.Add(GetElaspedTime(url));
+                 url = "http://localhost:5000/api/sync/" + i;
+                taskList.Add(GetElaspedTime(url,i));
             }
 
             await Task.WhenAll(taskList);
             s.Stop();
 
-            Console.WriteLine("RPS : " + (requestCount*1000) / s.ElapsedMilliseconds);
+          
+
+            foreach (var task in taskList)
+            {
+                var data = await task;
+                //Console.WriteLine($" {data.Item1} -> {data.Item2}");
+                System.IO.File.AppendAllText("d:\\logs.txt",$"{data.Item1},{data.Item2},{data.Item3}\n");
+            }
+
+            Console.WriteLine("RPS : " + (requestCount * 1000) / s.ElapsedMilliseconds);
 
 
         }
 
-        static async Task GetElaspedTime(string url)
+        static async Task<Tuple<int, long,int>> GetElaspedTime(string url,int number)
         {
-            var client = new HttpClient();    
-            await client.GetAsync(url);
-          
+            var s = new Stopwatch();
+            s.Start();
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+            s.Stop();
+            var threadCount=int.Parse(await result.Content.ReadAsStringAsync());
+            Console.WriteLine($"req:{number} | Threads: {threadCount} | Duration: {s.ElapsedMilliseconds}");
+            return new Tuple<int, long,int>(
+                threadCount,
+                s.ElapsedMilliseconds,number);
+
         }
     }
 }
